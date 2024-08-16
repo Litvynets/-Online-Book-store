ea6a5c356b660a20ef4fe341188b711dde204145
Ivan Litvynets <Litvyvanya@gmail.com>
2024-08-16 08:59:26 +0200

    Replaced RuntimeException to NoSuchElementEx

--- src/main/java/mate/academy/bookstore/repository/book/BookSpecificationProviderManager.java
+++ src/main/java/mate/academy/bookstore/repository/book/BookSpecificationProviderManager.java
@@ -1,6 +1,7 @@
 package mate.academy.bookstore.repository.book;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.SpecificationProvider;
@@ -15,7 +16,7 @@ public class BookSpecificationProviderManager implements SpecificationProviderMa
     @Override
     public SpecificationProvider<Book> getSpecificationProvider(String key) {
         return bookSpecificationProviders.stream().filter(p -> p.getKey().equals(key)).findFirst()
-                .orElseThrow(() -> new RuntimeException("Can't find correct specification"
+                .orElseThrow(() -> new NoSuchElementException("Can't find correct specification"
                         + " provider for key: " + key));
     }
 }
